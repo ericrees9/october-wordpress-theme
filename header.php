ce37bb4343d976f179b4711101ed509c4952e443
Eric Rees <erees@protonmail.com>
2025-02-11 20:12:47 -0500

    Aria additions to sidebar.

--- header.php
+++ header.php
@@ -12,13 +12,14 @@
         <div class="mobile-menu-btn">
             <input type="checkbox" id="mobile_menu_checkbox">
             <label for="mobile_menu_checkbox">
+                <span class="screen-reader-text">Toggle mobile menu</span>
                 <div></div>
                 <div></div>
                 <div></div>
             </label>
         </div>
         <aside class="sidebar" id="sidebar">
-            <nav class="site-navigation">
+            <nav class="site-navigation" aria-label="Primary Navigation">
                 <a href="<?php echo esc_url(home_url('/')); ?>" id="home-link">Home</a>
                 <?php
                     wp_nav_menu( array(
@@ -31,7 +32,7 @@
             <div class="nav-toggles">
                 <div class="social-wrapper">
                     <div class="bluesky">
-                        <a href="https://bsky.app/profile/b0nes.bsky.social">
+                        <a href="https://bsky.app/profile/b0nes.bsky.social" aria-label="Visit my Bluesky profile" rel="noopener noreferrer">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="30" height="30">
                                 <g transform="matrix(0.4383 0 0 0.4383 0.9744 1.432)">
                                     <path fill="#283618" d="M13.873 3.805C21.21 9.332 29.103 20.537 32 26.55v15.882c0-.338-.13.044-.41.867-1.512 4.456-7.418 21.847-20.923 7.944-7.111-7.32-3.819-14.64 9.125-16.85-7.405 1.264-15.73-.825-18.014-9.015C1.12 23.022 0 8.51 0 6.55 0-3.268 8.579-.182 13.873 3.805zm36.254 0C42.79 9.332 34.897 20.537 32 26.55v15.882c0-.338.13.044.41.867 1.512 4.456 7.418 21.847 20.923 7.944 7.111-7.32 3.819-14.64-9.125-16.85 7.405 1.264 15.73-.825 18.014-9.015C62.88 23.022 64 8.51 64 6.55c0-9.818-8.578-6.732-13.873-2.745z"/>
@@ -40,7 +41,7 @@
                         </a>
                     </div>
                     <div class="ig">
-                        <a href="https://www.instagram.com/ericrees/">
+                        <a href="https://www.instagram.com/ericrees/" aria-label="Visit my Instagram profile" rel="noopener noreferrer">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="30" height="30">
                                 <g transform="scale(0.1773)">
                                     <path d="M122.406,0H46.654C20.929,0,0,20.93,0,46.655v75.752c0,25.726,20.929,46.655,46.654,46.655h75.752
@@ -58,7 +59,7 @@
                         </a>
                     </div>
                     <div class="linkedin">
-                        <a href="https://www.linkedin.com/in/eric-rees/">
+                        <a href="https://www.linkedin.com/in/eric-rees/" aria-label="Visit my Linkedin profile" rel="noopener noreferrer">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35 35" width="35" height="35">
                                 <g transform="matrix(0.42164 0 0 0.42164 1.335 3.742)">
                                     <path fill="#283618" d="M72.865 61.1094a1.2 1.2 0 001.21-1.269c0-.9-.543-1.33-1.657-1.33h-1.8v4.712h.677v-2.054h.832l.019.025 1.291 2.029h.724l-1.389-2.1zm-.783-.472h-.785v-1.593h.995c.514 0 1.1.084 1.1.757 0 .774-.593.836-1.314.836m-16.873-5.433h-9.6v-15.034c0-3.585-.064-8.2-4.993-8.2-5 0-5.765 3.906-5.765 7.939v15.294h-9.6v-30.916h9.216v4.225h.129a10.1 10.1 0 019.093-4.994c9.73 0 11.524 6.4 11.524 14.726zm-40.79-35.143a5.571 5.571 0 115.57-5.572 5.571 5.571 0 01-5.57 5.572m4.8 35.143h-9.61v-30.917h9.61zm40.776-55.2H4.781A4.728 4.728 0 000 4.6744v55.439a4.731 4.731 0 004.781 4.675h55.21a4.741 4.741 0 004.8-4.675V4.6704a4.738 4.738 0 00-4.8-4.67"/>
@@ -68,7 +69,7 @@
                         </a>
                     </div>
                     <div class="github">
-                        <a href="https://github.com/ericrees9">
+                        <a href="https://github.com/ericrees9" aria-label="Visit my Github profile" rel="noopener noreferrer">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="30" height="30">
                                 <path fill-rule="evenodd" clip-rule="evenodd" d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" fill="#283618"/>
                             </svg>
@@ -80,7 +81,7 @@
                         <div class="dark-words">dark</div>
                         <div class="light-words">light</div>
                     </div>
-                    <label>
+                    <label aria-label="Toggle dark/light mode">
                         <input type ="checkbox">
                         <span class ="toggle-slider dark" id="dl-slider"></span>
                     </label>
